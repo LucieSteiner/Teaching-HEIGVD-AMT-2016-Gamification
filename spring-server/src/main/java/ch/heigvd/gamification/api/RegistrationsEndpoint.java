0d0c8abbea4d6946606655078d692f3a75abd300
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2016-11-28 22:56:10 +0100

    Fix issue with generated sources in pom.xml

--- spring-server/src/main/java/ch/heigvd/gamification/api/RegistrationsEndpoint.java
+++ spring-server/src/main/java/ch/heigvd/gamification/api/RegistrationsEndpoint.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Olivier Liechti
  */
-@RestController
+@RestController 
 public class RegistrationsEndpoint implements RegistrationsApi {
 
   @Override
